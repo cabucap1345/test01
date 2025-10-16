cc11fe28ab07ab31515f30ccdd563d127f8dbcb6
Cabucap1345 <tranmanhtrunga1k10@gmail.com>
2025-10-16 17:45:15 +0700

    commit

--- authen.js
+++ authen.js
@@ -1,4 +1,3 @@
-// A conceptual example of a JavaScript file for authentication.kjfhdksjok con des
 // This code is for educational purposes only and is not secure for production use.
 
 document.addEventListener('DOMContentLoaded', () => {
